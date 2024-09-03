c765eefbfa6ddbf82284a951f7516a0d955e2a97
Andrew Vasilchenko <cazonove@gmail.com>
2024-09-04 02:07:09 +0300

    изменил координаты в yandex-map.js; создар в partials card.html и занес туда карточки машин, сделав более простой и гибкий вызов card.html; поправил не дочеты; добавил ссылки к мессенджерам; изменил номера телефонов

--- src/js/components/yandex-map.js
+++ src/js/components/yandex-map.js
@@ -10,19 +10,19 @@ function init() {
 	});
 
 	var placemarks = [
-		new ymaps.Placemark([55.76, 37.64], {}, {
+		new ymaps.Placemark([55.579653, 37.591072], {}, {
 			iconLayout: 'default#image',
 			iconImageHref: './img/icons/location-map.webp',
 			iconImageSize: [64, 88],
 			iconImageOffset: [-32, -44],
 		}),
-		new ymaps.Placemark([55.709830, 37.390270], {}, {
+		new ymaps.Placemark([55.740624, 37.717285], {}, {
 			iconLayout: 'default#image',
 			iconImageHref: './img/icons/location-map.webp',
 			iconImageSize: [64, 88],
 			iconImageOffset: [-32, -44],
 		}),
-		new ymaps.Placemark([55.883198, 37.727249], {}, {
+		new ymaps.Placemark([55.847745, 37.558077], {}, {
 			iconLayout: 'default#image',
 			iconImageHref: './img/icons/location-map.webp',
 			iconImageSize: [64, 88],
